3b508e235bf5d9b05dd7b51796b140b4ae0118b1
FromtonRouge <fromtonrouge@gmail.com>
2016-02-24 00:35:55 +0100

    Keymap updated

--- keyboard/ergodox_ez/keymaps/fromtonrouge_qwerty/keymap.c
+++ keyboard/ergodox_ez/keymaps/fromtonrouge_qwerty/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [LAYER_DVORAK] = KEYMAP(  // layer 0 : default
         // left hand
-        S(KC_7),        KC_LBRC,    S(KC_LBRC), S(KC_RBRC), S(KC_9),    KC_EQL,     KC_DEL,
+        S(KC_7),        KC_LBRC,    S(KC_LBRC), S(KC_RBRC), S(KC_9),    KC_EQL,     KC_INS,
         S(KC_4),        KC_SCLN,    KC_COMM,    KC_DOT,     KC_P,       KC_Y,       KC_BSPC,
         KC_TAB,         KC_A,       KC_O,       KC_E,       KC_U,       KC_I,
         M(0),           KC_QUOT,    KC_Q,       KC_J,       KC_K,       KC_X,       KC_ESC,
