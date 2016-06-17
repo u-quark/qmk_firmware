4bcf12f686250fc739dc71aec82d341c988c49ea
FromtonRouge <fromtonrouge@gmail.com>
2016-06-17 14:05:45 +0200

    Tréma accent added

--- keyboard/ergodox_ez/keymaps/fromtonrouge/keymap.c
+++ keyboard/ergodox_ez/keymaps/fromtonrouge/keymap.c
@@ -455,7 +455,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                             KC_NO,      KC_NO,      KC_TRNS,  
        // right hand
                     KC_NO,          KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,
-                    KC_NO,          KC_NO,      KC_NO,      FR_UGRV,    KC_NO,          KC_NO,      KC_NO,  
+                    KC_NO,          KC_NO,      KC_NO,      FR_UGRV,    KC_NO,          LSFT(KC_LBRC),    KC_NO,  
                                     KC_7,       KC_2,       M(CIRC),    M(CIRC),        M(CIRC),    KC_NO,  
                     KC_NO,          KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,
                                                 KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,  
